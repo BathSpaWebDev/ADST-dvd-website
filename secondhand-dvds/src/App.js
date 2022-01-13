14ba28699e0aa198df4e4c3f6c4dc7b9e237363f
TZZ <tzvekare@gmail.com>
2022-01-13 22:47:05 +0000

    grid items

--- secondhand-dvds/src/App.js
+++ secondhand-dvds/src/App.js
@@ -2,12 +2,16 @@
 
 function App() {
   return (
-    <div className="text-2xl bg-gray-900 font-extralight text-white min-h-screen flex flex-col items-center py-10 gap-y-10
-    md:items-start xl:items-center md:flex-row md:h-max md:justify-around">
-      <div>
+    <div className="text-xl bg-slate-200 font-normal text-black min-h-screen flex flex-col items-center p-10 gap-y-10">
+      <div className="text-4xl flex-row bg-cyan-200 h-24 w-4/6 text-center pt-5 ">
       <h1>Second hand DVDs</h1>
       </div>
-      <div className= "md:flex-row bg-cyan-200" ></div>
+      <div className= "text-2xl bg-cyan-200 font-normal text-black h-4/6 w-4/6 flex flex-row items-center py-10 gap-y-10">
+
+        <div className="bg-white h-1/6 w4/6">
+
+        </div>
+      </div>
     </div>
   );
 }
