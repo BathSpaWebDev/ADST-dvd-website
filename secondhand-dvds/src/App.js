0d9af40ac8f0c60564925d71a88bf730c50ce7e6
aimaewood <aimaewood@gmail.com>
2022-01-13 12:27:25 +0000

    basket fullu functioning with dummy data

--- secondhand-dvds/src/App.js
+++ secondhand-dvds/src/App.js
@@ -1,22 +1,16 @@
 import {Basket} from "./components/Basket";
 import './App.css';
 
+
 function App() {
+
   return (
-      <Basket />
+      <div>
+          <Basket
+              movieTitle={"Midsommar"}
+              price={20} /> />
+      </div>
   )
 }
 
 export default App;
-
-
-{/*
-'add to basket' button should do the following:
-<button onClick={() => {
-    updateBasketList([...basketList, addItem(data.item.title)])
-    updateCost(costTotal + itemPrice)
-    updateItems(itemTotal + 1)
-    }
-}
-ALSO UPDATE STOCK NUMBER ON THAT COMPONENT?
-*/}
