b5185766c8bb0767c8f761c2da131425b4509ad9
TZZ <tzvekare@gmail.com>
2022-01-14 12:39:22 +0000

    component to list Dvds

--- secondhand-dvds/src/App.js
+++ secondhand-dvds/src/App.js
@@ -1,6 +1,8 @@
+import { DvdItems } from "./Components/DvdItem";
 
 
 function App() {
+  
   return (
     <div className="text-xl bg-slate-200 font-normal text-black min-h-screen flex flex-col items-center p-10 gap-y-10">
       <div className="text-4xl flex-row bg-cyan-200 h-24 w-4/6 text-center pt-5 ">
@@ -9,11 +11,11 @@ function App() {
       <div className= "text-2xl bg-cyan-200 font-normal text-black h-4/6 w-4/6 flex flex-row items-center py-10 gap-y-10">
 
         <div className="bg-white h-1/6 w4/6">
-
+<DvdItems />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
